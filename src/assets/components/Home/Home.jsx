c1311a120c0f7b65a4d548f3e0f2115b3ad24e55
Ezequiel Arevalo <ezequiel.arevalo@fravega.com.ar>
2025-04-06 17:19:27 -0300

    booking button added

--- src/assets/components/Home/Home.jsx
+++ src/assets/components/Home/Home.jsx
@@ -4,16 +4,16 @@ import { useTranslation } from "react-i18next";
 
 const Home = () => {
   // const { t, i18n } = useTranslation();
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
     return (
       <div id="section1" className="home">
         <div className="homeContent">
           <h1 className="home-title" >TOTAL <br></br> HOSTEL </h1>
           <p className="home-description">{t('home.text1')}<br></br></p>
           <div className="button">
-          {/* <button> 
+          <button> 
             <a href={`https://booking.totalhostel.com.ar/${i18n.language}`}> {t('home.book')} </a>
-          </button> */}
+          </button>
         </div>
         </div>
       </div>
